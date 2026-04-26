ae8fb47403ce4c06c5117d9ef7ec0c8dc258edd4
raz2001 <skmiraj1002@gmail.com>
2026-04-26 18:47:55 +0530

    medicine master done

--- mrts/src/app/features/medical-representative/pages/medicine-master/medicine-master.component.ts
+++ mrts/src/app/features/medical-representative/pages/medicine-master/medicine-master.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Package, ShoppingCart } from 'lucide-angular';
+
 
 @Component({
   selector: 'app-medicine-master',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class MedicineMasterComponent {
 
+  Package = Package;
+  ShoppingCart = ShoppingCart;
+
+  searchText = '';
+
+  products = [
+    {
+      name: 'Paracetamol 500mg',
+      category: 'Tablet',
+      composition: 'Paracetamol',
+      price: 50,
+      image: 'https://via.placeholder.com/300x200',
+      stockists: ['ABC Pharma', 'XYZ Distributors'],
+      qty: 1
+    },
+    {
+      name: 'Amoxicillin',
+      category: 'Capsule',
+      composition: 'Amoxicillin Trihydrate',
+      price: 120,
+      image: 'https://via.placeholder.com/300x200',
+      stockists: ['HealthCorp', 'MediSupply'],
+      qty: 1
+    }
+  ];
+
+  placeOrder(product: any) {
+    console.log('Order placed:', product);
+  }
 }
